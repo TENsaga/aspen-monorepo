10909792190e7385298593917b13e57c8c62db53
Aspen <munin@gmx.com>
2025-02-03 14:22:30 -0800

    Get tailwind and shadcn bundled correctly, import Button test inside aspen app

---
+++ packages/component-library/tailwind.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"], // Ensure Tailwind scans your components
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- packages/component-library/vite.config.ts
+++ packages/component-library/vite.config.ts
@@ -2,14 +2,32 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    libInjectCss(),
+    dts({ rollupTypes: false, tsconfigPath: "./tsconfig.app.json" }),
+  ],
   resolve: {
     alias: {
-      "@aspen-ui": path.resolve(__dirname, "src/main.tsx"),
+      "@component-library": path.resolve(__dirname, "src/main.ts"),
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/main.ts"),
+      name: "component-library",
+      formats: ["es"],
+      fileName: "component-library",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
     },
   },
 });
