f4ae904d33db19b86fbd6374a6e55effe7627a27
Aspen <munin@gmx.com>
2025-02-03 12:05:23 -0800

    Add new component library repo with tailwind and shadcn

---
+++ packages/component-library/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
